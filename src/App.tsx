6731a8aec902c8c7fc3276a75ed08e87090b1262
jimbolodisc <jimbolodisc@gmail.com>
2024-05-22 03:59:45 +0000

    wait for font

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
 import "./App.css";
 import LClogo from "./components/LClogo";
 import "./fonts.css";
+import React, { useState, useEffect } from "react";
+import FontFaceObserver from "fontfaceobserver";
 
 function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  useEffect(() => {
+    const font = new FontFaceObserver("SixtyFour");
+
+    font.load().then(() => {
+      setFontLoaded(true);
+    });
+  }, []);
+
   return (
     <div>
       <LClogo />
-      <div className="centerblock">
-        <h1 className="urlbanner">www.lodis-c.com</h1>
-        <p>where beauty is found...</p>
-      </div>
+      {fontLoaded ? (
+        <div className="centerblock">
+          <h1 className="urlbanner">www.lodis-c.com</h1>
+          <p>where beauty is found...</p>
+        </div>
+      ) : (
+        <div>Loading...</div>
+      )}
       <span className="copyright">&#169; 2024 Lodis C</span>
     </div>
   );
