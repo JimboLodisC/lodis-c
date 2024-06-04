b189204f77c8d242eaf245637e29924afe86557d
jimbolodisc <jimbolodisc@gmail.com>
2024-06-04 03:12:18 +0000

    cleanup and prune

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,14 @@
 import "./App.css";
 import LClogo from "./components/LClogo";
-import "./fonts.css";
-import React, { useState, useEffect } from "react";
-import FontFaceObserver from "fontfaceobserver";
 
 function App() {
-  const [fontLoaded, setFontLoaded] = useState(false);
-
-  useEffect(() => {
-    const font = new FontFaceObserver("SixtyFour");
-
-    font.load().then(() => {
-      setFontLoaded(true);
-    });
-  }, []);
-
   return (
     <div>
       <LClogo />
-      {fontLoaded ? (
-        <div className="centerblock">
-          <h1 className="urlbanner">www.lodis-c.com</h1>
-          <p>where beauty is found...</p>
-        </div>
-      ) : (
-        <div>Loading...</div>
-      )}
+      <div className="centerblock">
+        <h1 className="urlbanner">www.lodis-c.com</h1>
+        <p>where beauty is found...</p>
+      </div>
       <span className="copyright">&#169; 2024 Lodis C</span>
     </div>
   );
--- src/components/LClogo.tsx
+++ src/components/LClogo.tsx
@@ -1,16 +1,14 @@
 import "./LClogo.css";
 import lodiscLogo from "../assets/lclogo.svg";
 
-const LClogo = () => {
-  return (
-    <div className="lclogo">
-      <img
-        src={lodiscLogo}
-        alt="LC logo"
-        style={{ height: "90%", width: "inherit" }}
-      />
-    </div>
-  );
-};
+const LClogo = () => (
+  <div className="lclogo">
+    <img
+      src={lodiscLogo}
+      alt="LC logo"
+      style={{  height: "90%", width: "inherit" }}
+    />
+  </div>
+);
 
 export default LClogo;
